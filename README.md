927bb5b552bd7b6707c65cb03692cd8defa0f241
mug1wara26 <aloysiusgsq@gmail.com>
2025-03-24 18:22:41 +0800

    update readme

--- README.md
+++ README.md
@@ -2,14 +2,42 @@
 
 A LSP server for [source](https://github.com/source-academy/js-slang)
 
-# Setup
+## Use
 
-First run these commands:
+The code for the lsp is in `/server` but code for testing the lsp is provided in `/client`. If you want to use this lsp in your own VSCode extension, download the
+latest version from the [releases](https://github.com/mug1wara26/source-lsp/releases) page, and copy the set up in `client/src/extension.ts`.
+Ensure that the lines
 
-```console
-git clone https://github.com/mug1wara26/source-lsp
-cd source-lsp
-npm i
+```
+const serverModule = context.asAbsolutePath(
+  path.join("dist", 'source-lsp.js')
+);
 ```
 
-In vscode, press ctrl + shift + b to build, then press F5 to run the plugin. You can open the scripts in test_files for testing
+point to the correct path to the downloaded lsp file.
+
+If you want to test out the lsp, you can clone the repo and open up VSCode
+
+```console
+git clone https://github.com/mug1wara26/source-lsp && cd source-lsp
+npm i 
+code .
+```
+
+Then press ctrl + shift + b and enter to build, then press f5 to run the plugin.
+
+## Features
+
+Currently, the following language features are supported:
+
+* Completion
+* Go to declaration
+* Highlight names
+* Document Symbols
+* Rename symbol
+* Hover information
+* Diagnostics
+
+## Documentation
+
+Documentation for diagnostics can be found [here](server/src/rules)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,22 @@
 import {
-	createConnection,
-	TextDocuments,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	CompletionItem,
-	TextDocumentPositionParams,
-	TextDocumentSyncKind,
-	InitializeResult,
-	InsertTextFormat,
-	Range,
-	DocumentHighlightParams,
-	DocumentSymbolParams,
-	RenameParams,
-	WorkspaceEdit,
-	DocumentSymbol,
-	HoverParams,
-	Hover
+  createConnection,
+  TextDocuments,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+  InsertTextFormat,
+  Range,
+  DocumentHighlightParams,
+  DocumentSymbolParams,
+  RenameParams,
+  WorkspaceEdit,
+  DocumentSymbol,
+  HoverParams,
+  Hover
 } from 'vscode-languageserver/node';
 
 import { TextDocument } from 'vscode-languageserver-textdocument';
@@ -28,10 +28,10 @@ import { Chapter, Context } from './types';
 
 const SECTION = "\u00A7";
 const chapter_names = {
-	[`Source ${SECTION}1`]: Chapter.SOURCE_1,
-	[`Source ${SECTION}2`]: Chapter.SOURCE_2,
-	[`Source ${SECTION}3`]: Chapter.SOURCE_3,
-	[`Source ${SECTION}4`]: Chapter.SOURCE_4
+  [`Source ${SECTION}1`]: Chapter.SOURCE_1,
+  [`Source ${SECTION}2`]: Chapter.SOURCE_2,
+  [`Source ${SECTION}3`]: Chapter.SOURCE_3,
+  [`Source ${SECTION}4`]: Chapter.SOURCE_4
 };
 
 // Create a connection for the server, using Node's IPC as a transport.
@@ -43,157 +43,121 @@ let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 let hasConfigurationCapability: boolean = false;
 let hasWorkspaceFolderCapability: boolean = false;
-let hasDiagnosticRelatedInformationCapability: boolean = false;
 let context: Context = { chapter: Chapter.SOURCE_1 };
 
 let astCache: Map<string, AST> = new Map();
 
 function getAST(uri: string): AST {
-	if(astCache.has(uri)) return astCache.get(uri)!;
+  if (astCache.has(uri)) return astCache.get(uri)!;
 
-	const ast = new AST(documents.get(uri)!.getText(), context, uri);
-	astCache.set(uri, ast);
-	return ast;
+  const ast = new AST(documents.get(uri)!.getText(), context, uri);
+  astCache.set(uri, ast);
+  return ast;
 }
 
 connection.onInitialize((params: InitializeParams) => {
-	let capabilities = params.capabilities;
+  let capabilities = params.capabilities;
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-	hasWorkspaceFolderCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.workspaceFolders
-	);
-	hasDiagnosticRelatedInformationCapability = !!(
-		capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation
-	);
+  // Does the client support the `workspace/configuration` request?
+  // If not, we fall back using global settings.
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  );
 
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			// Tell the client that this server supports code completion.
-			completionProvider: {
-				resolveProvider: true
-			},
-			declarationProvider: true,
-			documentHighlightProvider: true,
-			documentSymbolProvider: true,
-			renameProvider: true,
-			hoverProvider: true
-		}
-	};
-	if (hasWorkspaceFolderCapability) {
-		result.capabilities.workspace = {
-			workspaceFolders: {
-				supported: true
-			}
-		};
-	}
-	return result;
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true
+      },
+      declarationProvider: true,
+      documentHighlightProvider: true,
+      documentSymbolProvider: true,
+      renameProvider: true,
+      hoverProvider: true,
+      diagnosticProvider: {
+        interFileDependencies: false,
+        workspaceDiagnostics: false
+      }
+    }
+  };
+  if (hasWorkspaceFolderCapability) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true
+      }
+    };
+  }
+  return result;
 });
 
 connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes.
+    connection.client.register(DidChangeConfigurationNotification.type, undefined);
+  }
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders(_event => {
+      connection.console.log('Workspace folder change event received.');
+    });
+  }
 });
-
-// The example settings
-interface ExampleSettings {
-	maxNumberOfProblems: number;
-}
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
-
-// Cache the settings of all open documents
-let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
-
-connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.languageServerExample || defaultSettings)
-		);
-	}
-
-	// Revalidate all open text documents
-	documents.all().forEach(validateTextDocument);
-});
-
 
 // Custom request to set the language version
 connection.onRequest("setLanguageVersion", (params: { version: string }) => {
-	if (Object.keys(chapter_names).includes(params.version)) {
-		context = { chapter: chapter_names[params.version as keyof typeof chapter_names] }
-		astCache.clear();
-		documents.all().forEach(validateTextDocument);
-		return { success: true };
-	}
-	else return {success: false};
-});
-
-// Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
+  if (Object.keys(chapter_names).includes(params.version)) {
+    context = { chapter: chapter_names[params.version as keyof typeof chapter_names] }
+    astCache.clear();
+    documents.all().forEach(validateTextDocument);
+    return { success: true };
+  }
+  else return { success: false };
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 let timeout: NodeJS.Timeout | undefined = undefined;
 documents.onDidChangeContent(change => {
-	if (timeout) clearTimeout(timeout); 
-	timeout = setTimeout(() => {
-		astCache.delete(change.document.uri);
-		validateTextDocument(change.document);
-	}, 300);
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    astCache.delete(change.document.uri);
+    validateTextDocument(change.document);
+  }, 300);
 });
 
 async function validateTextDocument(document: TextDocument): Promise<void> {
-	// In this simple example we get the settings for every validate run.
-	connection.sendDiagnostics({ uri: document.uri, diagnostics: getAST(document.uri).getDiagnostics()});
+  connection.sendDiagnostics({ uri: document.uri, diagnostics: getAST(document.uri).getDiagnostics() });
 }
 
+// TODO: handle file deletion and creation 
 connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VS Code
-	connection.console.log('We received a file change event');
+  // Monitored files have change in VS Code
+  connection.console.log('We received a file change event');
 });
 
 // This handler provides the initial list of the completion items.
 connection.onCompletion(
-	async (textDocumentPosition: TextDocumentPositionParams): Promise<CompletionItem[]> => {
-		const document = documents.get(textDocumentPosition.textDocument.uri);
-		if (!document) return [];
+  async (textDocumentPosition: TextDocumentPositionParams): Promise<CompletionItem[]> => {
+    const document = documents.get(textDocumentPosition.textDocument.uri);
+    if (!document) return [];
 
-		return getAST(textDocumentPosition.textDocument.uri).getCompletionItems(textDocumentPosition.position);
-	}
+    return getAST(textDocumentPosition.textDocument.uri).getCompletionItems(textDocumentPosition.position);
+  }
 );
 
 // This handler resolves additional information for the item selected in
 // the completion list.
 connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-	if (item.data.parameters) {
-		item.insertText = `${item.label}(${item.data.parameters.filter((x: string) => item.data.optional_params ? !item.data.optional_params.includes(x) : true).map((param: string, idx: number) => `\${${idx+1}:${param}}`)})`;
-		item.insertTextFormat = InsertTextFormat.Snippet;
-	};
+  if (item.data.parameters) {
+    item.insertText = `${item.label}(${item.data.parameters.filter((x: string) => item.data.optional_params ? !item.data.optional_params.includes(x) : true).map((param: string, idx: number) => `\${${idx + 1}:${param}}`)})`;
+    item.insertTextFormat = InsertTextFormat.Snippet;
+  };
 
-	return item;
+  return item;
 }
 );
 
@@ -201,50 +165,50 @@ connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
 
 // This handler provides the declaration location of the name at the location provided
 connection.onDeclaration(async (params) => {
-	const position = params.position;
+  const position = params.position;
 
-	const result = getAST(params.textDocument.uri).findDeclaration(position);
+  const result = getAST(params.textDocument.uri).findDeclaration(position);
 
-	if (result) {
-		const range: Range = sourceLocToRange(result);
+  if (result) {
+    const range: Range = sourceLocToRange(result);
 
-		return {
-			uri: params.textDocument.uri,
-			range
-		};
-	}
-	else {
-		return null;
-	}
+    return {
+      uri: params.textDocument.uri,
+      range
+    };
+  }
+  else {
+    return null;
+  }
 });
 
 connection.onDocumentHighlight((params: DocumentHighlightParams) => {
-	const document = documents.get(params.textDocument.uri);
-	if (!document) return null;
+  const document = documents.get(params.textDocument.uri);
+  if (!document) return null;
 
-	return getAST(params.textDocument.uri).getOccurences(params.position);
+  return getAST(params.textDocument.uri).getOccurences(params.position);
 })
 
-connection.onDocumentSymbol(async (params: DocumentSymbolParams) : Promise<DocumentSymbol[] | null> => {
-	return getAST(params.textDocument.uri).getDocumentSymbols();
+connection.onDocumentSymbol(async (params: DocumentSymbolParams): Promise<DocumentSymbol[] | null> => {
+  return getAST(params.textDocument.uri).getDocumentSymbols();
 })
 
 connection.onRenameRequest((params: RenameParams): WorkspaceEdit | null => {
-	const document = documents.get(params.textDocument.uri);
-	if (!document) return null;
+  const document = documents.get(params.textDocument.uri);
+  if (!document) return null;
 
-	const position = params.position;
+  const position = params.position;
 
-	return getAST(params.textDocument.uri).renameSymbol(position, params.newName);
+  return getAST(params.textDocument.uri).renameSymbol(position, params.newName);
 })
 
 connection.onHover((params: HoverParams): Hover | null => {
-	const document = documents.get(params.textDocument.uri);
-	if (!document) return null;
+  const document = documents.get(params.textDocument.uri);
+  if (!document) return null;
 
-	const position = params.position;
+  const position = params.position;
 
-	return getAST(params.textDocument.uri).onHover(position);
+  return getAST(params.textDocument.uri).onHover(position);
 })
 
 
